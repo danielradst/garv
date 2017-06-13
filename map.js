c8977d13691f499e865a233f6f1f7e9233f2ad26
Daniel Stoyanov <daniel.radst@gmail.com>
2017-06-13 23:49:19 +0300

    v1

---
+++ map.js
@@ -0,0 +1,11 @@
+function initMap() {
+    var uluru = {lat: 43.14260, lng: 23.36530};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
